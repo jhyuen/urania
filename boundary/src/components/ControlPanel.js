f67b6607366406f2f640f52b877a3c1334979af3
Joseph Yuen <josephnewbox@gmail.com>
2020-10-28 14:32:34 -0400

    added buttons

--- boundary/src/components/ControlPanel.js
+++ boundary/src/components/ControlPanel.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import './ControlPanel.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 class ControlPanel extends Component {
 	state = {}
 	render() {
 		return(
-			<p>Control Panel</p>
+			<>
+				<h1>Control Panel</h1>
+				<Button variant="primary" onClick={this.viewAsViewer}>View as Viewer</Button>{' '}
+				<Button variant="primary" onClick={this.viewSnippet}>View another Snippet</Button>{' '}
+				<Button variant="primary" onClick={this.createSnippet}>Create another Snippet</Button>{' '}
+				<Button variant="primary" onClick={this.deleteSnippet}>Delete Snippet</Button>{' '}
+			</>
 		)
+	}
+	
+	viewAsViewer() {
+		console.log("view as viewer");
+	}
+	
+	viewSnippet() {
+		console.log("view snippet");
+	}
+	
+	createSnippet() {
+		console.log("created snippet");
+	}
+	
+	deleteSnippet() {
+		console.log("deleted snippet");
 	}
 }
 
