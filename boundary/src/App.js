40c063d98a6ccc05475d9b81123590aa1dcc49e3
Vakhter Vladimir <vladimir.vakhter@gmail.com>
2020-10-29 14:56:57 -0400

    add switch, get rid of React.Fragment

--- boundary/src/App.js
+++ boundary/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import CreatorView from './components/views/CreatorView';
 import ViewerView from './components/views/ViewerView';
@@ -19,21 +19,11 @@ class App extends Component {
 	render() {
 		return (
 			<Router>
-				<Route exact path="/" render={props => (
-					<React.Fragment>
-						<ViewerView />
-					</React.Fragment>
-				)} />
-				<Route path="/creator" render={props => (
-					<React.Fragment>
-						<CreatorView />
-					</React.Fragment>
-				)} />
-				<Route path="/admin" render={props => (
-					<React.Fragment>
-						<AdminView />
-					</React.Fragment>
-				)} />
+				<Switch>
+					<Route exact path="/" 	component={ ViewerView } />
+					<Route path="/creator" 	component={ CreatorView } />
+					<Route path="/admin" 	component={ AdminView } />
+				</Switch>
 			</Router>
 		);
 	}
