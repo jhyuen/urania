bd7c3d1aced5bfe78956fb02b0b8b68db4b39edb
Joseph Yuen <josephnewbox@gmail.com>
2020-11-16 00:35:47 -0500

    got rid of warning about classname

--- boundary/src/components/elements/Loader.js
+++ boundary/src/components/elements/Loader.js
@@ -4,12 +4,12 @@ class Loader extends Component {
 	
 	render() {
 		return (
-<div class="primary">
-<div class="content">
-   <div class="planet">
-      <div class="ring"></div>
-         <div class="cover-ring"></div>
-      <div class="spots">
+<div className="primary">
+<div className="content">
+   <div className="planet">
+      <div className="ring"></div>
+         <div className="cover-ring"></div>
+      <div className="spots">
          <span></span>
          <span></span>
          <span></span>
