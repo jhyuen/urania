98fdd620fcc3f4d53f7557ddb4aeeab2d63076f1
Vakhter Vladimir <vladimir.vakhter@gmail.com>
2020-11-20 11:36:34 -0500

    move the admin table into a separate file

--- boundary/src/components/views/AdminView1.js
+++ boundary/src/components/views/AdminView1.js
@@ -1,132 +1,40 @@
-import React from 'react'
-import { useTable, usePagination } from 'react-table'
+import React, { Component } from 'react';
+import AdminTable from '../elements/AdminTable.js';
 import './AdminView1.css';
 
-import Button from 'react-bootstrap/Button';
-import Badge from 'react-bootstrap/Badge';
-
-import FirstPage from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPage from '@material-ui/icons/LastPage';
-
 import makeData from './makeData'
-
-// fetchData - a method to fetch new data when pagination state changes
-// we can also add a loading state to let our table know it's loading new data
-function Table({ columns, data, fetchData, loading, pageCount: controlledPageCount }) 
-{
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    page,
-    canPreviousPage,
-    canNextPage,
-    pageOptions,
-    pageCount,
-    gotoPage,
-    nextPage,
-    previousPage,
-    setPageSize,
-    state: { pageIndex, pageSize }			// get the state from the instance
-  } = useTable ({
-      columns,
-      data,
-      initialState: { pageIndex: 0 },
-      manualPagination: true, 				// tell usePagination that we will handle our own data fetching.
-      										// this means we'll also have to provide our own pageCount.
-      pageCount: controlledPageCount,
-    },
-    usePagination
-  )
-
-  // listen for changes in pagination and use the state to fetch our new data
-  React.useEffect(() => { fetchData({ pageIndex, pageSize }) }, [fetchData, pageIndex, pageSize])
-
-  // render the UI for your table
-  return (
-    <>
-		<table {...getTableProps()} className="adminTable">
-			<thead className="tableHead">
-				{headerGroups.map(headerGroup => (
-					<tr {...headerGroup.getHeaderGroupProps()} >
-					    {headerGroup.headers.map(column => (
-							<th {...column.getHeaderProps()} >
-					            {column.render('Header')}
-					            <span>{column.isSorted ? column.isSortedDesc ? 'v' : '^' : ''}</span>
-							</th>
-					    ))}
-					</tr>
-				))}
-	        </thead>
-
-	        <tbody {...getTableBodyProps()} className="tableBody">
-				{page.map((row, i) => {
-					prepareRow(row)
-					return (
-						<tr {...row.getRowProps()}>
-							{row.cells.map(cell => {
-								return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-							})}
-						</tr>
-					)
-				})}
-				<tr>
-					{loading ? (<td colSpan="10000">Loading...</td>) : 	// use custom loading state to show a loading indicator
-					(<td colSpan="10000">Showing {page.length} of {controlledPageCount * pageSize} results</td>)}
-				</tr>
-	        </tbody>
-      </table>
-
-      <div className="pagination">
-		<Button className='btnFirstPage' variant="dark" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-			<FirstPage/>
-		</Button>
-		
-		<Button className='btnPrevPage' variant="dark" onClick={() => previousPage()} disabled={!canPreviousPage}>
-			<KeyboardArrowLeft/>
-		</Button>
-		
-		<Button className='btnNextPage' variant="dark" onClick={() => nextPage()} disabled={!canNextPage}>
-			<KeyboardArrowRight/>
-		</Button>
-		
-		<Button className='btnLastPage' variant="dark" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-			<LastPage/>
-		</Button>
-
-		<Badge className='pagesBadge' variant="primary">
-			Page{' '}
-			{pageIndex + 1} of {pageOptions.length}
-		</Badge>
-
-		<Badge className='goToBadge' variant="primary">
-          Go to page:{' '}
-          <input className='goToInput' type="number" defaultValue={pageIndex + 1}
-            onChange={ e => {const page = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(page) }}
-          />
-		</Badge>
-
-        <select className='recordsPerPage' value={pageSize} onChange={ e => {setPageSize(Number(e.target.value))} }>
-			{[10, 20, 30, 40, 50].map(pageSize => (
-			<option key={pageSize} value={pageSize}>
-			  Show {pageSize}
-			</option>
-			))}
-		</select>
-      </div>
-    </>
-  )
-}
 
 // get data
 const serverData = makeData(10000)
 
+/*const serverData = async () => {
+	var get_snippets_url = "https://e061bpd3ph.execute-api.us-east-2.amazonaws.com/beta/snippetList"; 
+	var data = await fetch(get_snippets_url)
+	var snippetData = await data.json()
+	//console.log(snippetData.viewerPasswordStatus)
+	console.log(snippetData.list)
+	return snippetData.list
+}*/
+
+/*function makeData(...lens) {
+  const makeDataLevel = (depth = 0) => {
+    const len = lens[depth]
+    return range(len).map(d => {
+      return {
+        ...newPerson(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+      }
+    })
+  }
+
+  return makeDataLevel()
+}*/
+
+
 function AdminView1() {
-  const columns = React.useMemo(
+  	
+
+	const columns = React.useMemo(
     () => [
 			{
 				Header	: 'SnippetID',
@@ -161,27 +69,27 @@ function AdminView1() {
 	
 	    // set the loading state
 	    setLoading(true)
-	
-    	// simulate the server delay
-	    setTimeout(() => {
-	      // Only update the data if this is the latest fetch
-	      if (fetchId === fetchIdRef.current) {
-	        const startRow = pageSize * pageIndex
-	        const endRow = startRow + pageSize
-	        setData(serverData.slice(startRow, endRow))
-	
-	        // Your server could send back total page count.
-	        // For now we'll just fake it, too
-	        setPageCount(Math.ceil(serverData.length / pageSize))
-	
-	        setLoading(false)
-	      }}, 1000)
+	    
+		// Only update the data if this is the latest fetch
+		if (fetchId === fetchIdRef.current) {
+		  const startRow = pageSize * pageIndex
+		  const endRow = startRow + pageSize
+
+		  setData(serverData.slice(startRow, endRow))
+		
+		  // Your server could send back total page count.
+		  // For now we'll just fake it, too
+		  setPageCount(Math.ceil(serverData.length / pageSize))
+		
+		  setLoading(false)
+		}
+		
   }, [])
 
   return (
 	<div>
 		<h1>Admin View</h1> 
-		<Table
+		<AdminTable
 	        columns		= { columns }
 	        data		= { data }
 	        fetchData	= { fetchData }
@@ -193,3 +101,68 @@ function AdminView1() {
 }
 
 export default AdminView1;
+
+class AdminView2 extends Component {
+	state = {
+		data: []
+		
+	
+	}
+
+	componentDidMount() { this.fetchAllSnippets() }
+
+	fetchAllSnippets = async () => {
+		var get_snippets_url = "https://e061bpd3ph.execute-api.us-east-2.amazonaws.com/beta/snippetList"; 
+		var data = await fetch(get_snippets_url)
+		var snippetData = await data.json()
+		//console.log(snippetData.viewerPasswordStatus)
+		console.log(snippetData.list)
+		this.setState({ data: snippetData.list })
+	}
+	
+	render() {
+		const columns = React.useMemo(
+		    () => [
+					{
+						Header	: 'SnippetID',
+						accessor: 'snippetID',
+					},
+					{
+						Header	: 'Date Created',
+						accessor: 'viewerPassword',		// TODO: change to 'timeStamp'
+					},
+					{
+						Header	: 'Days Old',
+						accessor: 'daysold',
+					},
+					{
+						Header	: 'Delete',
+						accessor: 'delete',
+					}
+		        ]
+		  )
+		
+		const [data, setData] 			= React.useState([])
+		const [loading, setLoading] 	= React.useState(false)
+		const [pageCount, setPageCount] = React.useState(0)
+		const fetchIdRef 				= React.useRef(0)
+		
+		
+		return (
+			<div>
+				<h1>Admin View</h1> 
+				<AdminTable
+			        columns		= { columns }
+			        data		= { data }
+			        fetchData	= { fetchData }
+			        loading		= { loading }
+			        pageCount	= { pageCount }
+			      />
+			</div>
+		)
+	}
+}
+
+export default AdminView2;
+
+
